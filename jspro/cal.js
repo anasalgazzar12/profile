e67b70593b6be3ed759b6fd2c7e7c3520872db4e
anasalgazzar12 <106516246+anasalgazzar12@users.noreply.github.com>
2022-06-10 11:24:12 +0300

    Add files via upload

---
+++ jspro/cal.js
@@ -0,0 +1,58 @@
+/*
+var tweet = document.getElementById(tweet);
+
+var tweetbtn=document.getElementn
+
+  let numOfTweet = 1 ;
+
+  var tweetArea =document.getElementById("tweetArea");
+   
+  const newUserInfo = document.getElementById("tweetUserInfo")
+
+
+
+function mackTweet(){
+  numOfTweet ++;
+  let newTwet = document.getElementById("tweet1");
+   newTwet.className("tweet");
+   newTwet.id(`tweet${numOfTweet}`);
+   //newTweeet contant  
+   let newP=document.getElementById("userText");
+   newP.textContent=document.getElementById("tweetText") ;
+   newTwet.innerHTML=
+     newUserInfo && newP; 
+      return newTwet
+}
+
+
+tweetbtn.addEventListener("click")
+  alert("are yuo sure it's all the tweet");
+  tweetArea.innerHTML= mackTweet();
+*/
+var tweetArea =[];
+
+var tweetnum=1;
+
+let tweetText  ;
+tweetText=document.getElementById("textarea").ariaValueText;
+
+let newUserInfo  ; 
+newUserInfo=document.getElementById("tweetUserInfo");
+
+var newTweet= {
+ n: newUserInfo,
+ t:tweetTextVal 
+};
+var tweetDiv = document.createElement("div");
+
+var btn = document.getElementsByName("btn1");
+
+btn.add
+
+function pushInTweetArea () {
+tweetArea.push(newTweet);
+return tweetDiv.innerHTML = tweetArea.values;
+}
+
+
+
